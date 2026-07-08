2b914da6f85af2bb997fcabdd56c3243e496db3f
JDR11 <43808975+JDR11@users.noreply.github.com>
2026-07-08 22:11:36 +0100

    Removed remaining traces of example mod + begun work on adding PhantomItemSlot to GUI

--- src/main/java/com/JDR11/ae2debugtools/client/gui/GuiHandler.java
+++ src/main/java/com/JDR11/ae2debugtools/client/gui/GuiHandler.java
@@ -3,18 +3,22 @@ package com.JDR11.ae2debugtools.client.gui;
 import static com.JDR11.ae2debugtools.Config.openInterface;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.util.ChatComponentText;
 
 import com.JDR11.ae2debugtools.Ae2DebugTools;
 import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.factory.ClientGUI;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.ModularScreen;
+import com.cleanroommc.modularui.utils.item.IItemHandler;
+import com.cleanroommc.modularui.utils.item.ItemStackHandler;
+import com.cleanroommc.modularui.widgets.slot.PhantomItemSlot;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
 
 public class GuiHandler {
+
+    private final IItemHandler invHandler = new ItemStackHandler(1);
 
     @SubscribeEvent
     public void openGui(InputEvent.KeyInputEvent event) {
@@ -30,12 +34,14 @@ public class GuiHandler {
     }
 
     public static ModularScreen createGui() {
+
         ModularPanel panel = ModularPanel.defaultPanel("tutorial");
         panel.child(
             IKey.str("My first screen")
                 .asWidget()
                 .top(7)
-                .left(7));
+                .left(7))
+            .child(new PhantomItemSlot().slot(invHandler, 1));
 
         return new ModularScreen(panel);
     }
